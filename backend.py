08ce8af39d520078501df5bb870db480c80a8f52
pepe44cem <114621427+pepe44cem@users.noreply.github.com>
2023-10-26 12:43:55 -0600

    A ver si sale

---
+++ backend.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+from flask_cors import CORS
+import joblib
+
+app = Flask(_name_)
+
+@app.route("hola/", methods = ["GET"])
+def inicio():
+    return "Hola mundo"
+
+if _name_ == "_main_":
+    app.run(host = "0.0.0.0", debug = False, port = 8081)
