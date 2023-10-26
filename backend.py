33b10ec37b228a8b29a1ab7295eea82e0bd996b9
pepe44cem <114621427+pepe44cem@users.noreply.github.com>
2023-10-26 13:28:53 -0600

    A ver si sale F10

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 
-dt = joblib.load("./static/dt2.joblib")
+dt = joblib.load("./static/final.joblib")
 
 app = Flask(__name__)
 CORS(app)
@@ -14,6 +14,19 @@ def inicio():
 @app.route("/predict_json", method = ["POST"])
 def predict_json():
     data = request.json
+    x = [[
+        float(data["pH"]),
+        float(data["sulphates"]),
+        float(data["alcohol"])
+        ]]
+    
+    y_pred = dt.predict(x)
+    print(y_pred)
+    return jsonify({"result": y_pred[0]})
+
+@app.route("/predict_form", method = ["POST"])
+def predict_form():
+    data = request.form
     x = [[
         float(data["pH"]),
         float(data["sulphates"]),
